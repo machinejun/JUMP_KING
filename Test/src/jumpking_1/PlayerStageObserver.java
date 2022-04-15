6186e32ca3070c63bd1d1e4c2452bf987d0cdd8c
machinejun <akswns03@naver.com>
2022-04-16 00:22:32 +0900

    수정

--- Test/src/jumpking_1/PlayerStageObserver.java
+++ Test/src/jumpking_1/PlayerStageObserver.java
@@ -16,15 +16,40 @@ public class PlayerStageObserver implements Runnable {
 		while (true) {
 			int i = 1;
 			if( 0 <= i && i <= 5) {
-				if (player.getY() <= 50 && backgroundMap.getStageNum() == i) {
-					backgroundMap.setStageNum(++i);
+				if (player.getY() <= 50 && backgroundMap.getStageNum() == 1) {
+					backgroundMap.setStageNum(2);
 					backgroundMap.changeStage();
 					player.setY(910);
-				} else if (player.getY() >= 950 && backgroundMap.getStageNum() == i) {
-					backgroundMap.setStageNum(--i);
+				} else if (player.getY() >= 950 && backgroundMap.getStageNum() == 2) {
+					backgroundMap.setStageNum(1);
+					backgroundMap.changeStage();
+					player.setY(70);	
+				} else if (player.getY() <= 50 && backgroundMap.getStageNum() == 2) {
+					backgroundMap.setStageNum(3);
+					backgroundMap.changeStage();
+					player.setY(910);
+				} else if (player.getY() >= 950 && backgroundMap.getStageNum() == 3) {
+					backgroundMap.setStageNum(2);
+					backgroundMap.changeStage();
+					player.setY(70);	
+				} else if (player.getY() <= 50 && backgroundMap.getStageNum() == 3) {
+					backgroundMap.setStageNum(4);
+					backgroundMap.changeStage();
+					player.setY(910);
+				} else if (player.getY() >= 950 && backgroundMap.getStageNum() == 4) {
+					backgroundMap.setStageNum(3);
+					backgroundMap.changeStage();
+					player.setY(70);	
+				} else if (player.getY() <= 50 && backgroundMap.getStageNum() == 4) {
+					backgroundMap.setStageNum(5);
+					backgroundMap.changeStage();
+					player.setY(910);
+				} else if (player.getY() >= 950 && backgroundMap.getStageNum() == 5) {
+					backgroundMap.setStageNum(4);
 					backgroundMap.changeStage();
 					player.setY(70);	
 				} 
+			
 			}
 			
 			// 5스테이지 일때 조건
